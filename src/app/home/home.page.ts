e05c38b692780627a74d5e55a7bdee6d40a21130
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-07 12:59:18 +0200

    creacion app android BoosterHub

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  url = "https://boosterhub.up.railway.app/";
+
+  browser = inject(InAppBrowser);
+
+  ngOnInit(): void {
+
+    const browserInstance = this.browser.create(
+      this.url,
+      '_self',
+      'location=no, hidenavigationbuttons=true, hideurlbar=true, zoom=no'
+    );
+
+    browserInstance.on('exit').subscribe((ev) => {
+      App.exitApp();
+    });
+  }
 
 }
