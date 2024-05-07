e618ccedee5f951ad2ce4fd3ecea6c2f97e6ac8f
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-07 10:50:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BoosterHub-app',
+  webDir: 'www'
+};
+
+export default config;
