c1b114ef314792fd66b3ec92a35e91b860b214f8
PalmerinErwerle <franiFJRF@gmail.com>
2024-05-10 14:20:00 +0200

    bimplementacion bloqueo pantalla vertical

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 import { App } from '@capacitor/app';
 
 @Component({
@@ -12,8 +13,11 @@ export class HomePage implements OnInit {
   url = "https://boosterhub.up.railway.app/";
 
   browser = inject(InAppBrowser);
+  screen = inject(ScreenOrientation);
 
   ngOnInit(): void {
+
+    this.screen.lock(this.screen.ORIENTATIONS.PORTRAIT);
 
     const browserInstance = this.browser.create(
       this.url,
